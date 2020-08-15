75aaf2b08a3c43bced76ce70d5f3efef5f09bf2b
mattomolewa <matthew.omolewa@gmail.com>
2020-08-15 17:12:28 +0100

    added description

--- 3- Dom & Events excercise/todo_list - part3&4/script.js
+++ 3- Dom & Events excercise/todo_list - part3&4/script.js
@@ -1,3 +1,17 @@
+/*Part 3
+
+For this assignment you will be combining your knowledge of DOM access and events to build a todo app!
+
+As a user, you should be able to:
+
+    Add a new todo (by submitting a form)
+    Mark a todo as completed (cross out the text of the todo)
+    Remove a todo
+
+Part 4:
+
+Using localStorage, try to store your todos so that if you refresh the page you do not lose what you have added to the list! As a super bonus, try to also save todos that you have marked as complete! */
+
 const list = document.querySelector("ul");
 const form = document.querySelector("form");
 //adding todos
